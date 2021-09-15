3daa31ac9ae40681516bbdb59f74c1f47141a0c1
hwookim <gusdn9754@gmail.com>
2021-09-15 22:00:05 +0900

    chore: Apply typescript with vite
    - Set eslint options
      - Apply typescript plugin, parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,16 @@ module.exports = {
     "eslint:recommended",
     "plugin:prettier/recommended",
     "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
+    parser: "@typescript-eslint/parser",
   },
-  plugins: ["vue"],
+  plugins: ["vue", "@typescript-eslint"],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
